52d0e53f010a6361bbf18133bb23c13803b0c333
wangruo <121095806@qq.com>
2024-06-18 19:49:34 +0800

    1. 完成

---
+++ beer_song/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+        1 => format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n),
+        2 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n, n, n - 1),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n - 1)
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    let mut s = String::new();
+
+    for i in (end..=start).rev() {
+        s.push_str(&verse(i));
+        if i != end { s.push('\n'); }
+    }
+
+    s
+}
